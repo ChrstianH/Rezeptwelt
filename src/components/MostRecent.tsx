2af19627cb13804e8498237fdb60d29eac8844c2
ChrstianH <henschel_christian@web.de>
2026-03-25 21:06:12 +0100

    load most Recentr Recipes

--- src/components/MostRecent.tsx
+++ src/components/MostRecent.tsx
@@ -1,31 +1,25 @@
-import RecipePage from "../pages/RecipePage";
-import { QueryData } from "@supabase/supabase-js";
+import RecipesData from "../types/recipesData";
 import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabase";
+import axios from "axios";
+import RecipeCard from "./RecipeCard";
 
 export default function MostRecent() {
-  const [mostRecent, setMostRecent] = useState<RecipesData>([]);
+  const [mostRecent, setMostRecent] = useState<RecipesData[]>();
 
-  const getMostRecentRecipes = async () => {
-    const recipes = await supabase
-      .from("recipes")
-      .select("id, image_url, name, description")
-      .order("created_at", { ascending: false })
-      .limit(3);
-    return recipes;
-  };
   useEffect(() => {
-    getMostRecentRecipes().then((result) => setMostRecent(result.data ?? []));
+    axios
+      .get("https://rezeptwelt-backend.onrender.com/getMostRecent")
+      .then((recipes) => {
+        setMostRecent(recipes.data ?? []);
+      });
   }, []);
-
-  type RecipesData = QueryData<ReturnType<typeof getMostRecentRecipes>>;
 
   return (
     <div className="most-recent">
       <h3>Die neuesten Rezepte</h3>
 
-      {mostRecent?.map((recipe) => (
-        <RecipePage key={recipe.id} recipe={recipe} />
+      {mostRecent?.map((recipe: RecipesData) => (
+        <RecipeCard key={recipe.id} recipe={recipe} />
       ))}
     </div>
   );
